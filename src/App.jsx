270a32574ef3cfb3b7d8cc8d7f6057c28bbb3ede
Uulkelsin Bektemirova <100680512+ukatls@users.noreply.github.com>
2022-05-24 12:08:28 +0600

    feat: header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Main from './components/main/Main';
 import Header from './components/header/Header';
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import logo from '../images/logo.svg'
-import iconMan from '../images/icon-man.svg'
+import React from "react";
+import logo from "../images/logo.svg";
+import iconMan from "../images/icon-man.svg";
+import css from "./Header.module.css";
+
 export default function Header() {
-  return (
-    <div className='container'>
-        <div>
-            <img src={logo} alt="#" />
-            <span>Главная</span>
-            <span>О проекте</span>
+  return (  
+    <div className="container">
+      <div className={css.header}>
+        <div className={css.right}>
+          <img src={logo} alt="#" />
+          <span>Главная</span>
+          <span>О проекте</span>
         </div>
-        <div>
-            <input type="search" name="" id="" />
-            <img src={iconMan} alt="#" />
+        <div className={css.left}>
+          <input type="search" name="" id="" />
+          <img src={iconMan} alt="#" />
         </div>
+      </div>
     </div>
-  )
+  );
 }
